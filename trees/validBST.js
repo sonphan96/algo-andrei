2dc552c096300a0f738791a378f18bdb82e60737
sonphan96 <phantruongson96@gmail.com>
2021-07-01 16:17:42 -0400

    valid BST

---
+++ trees/validBST.js
@@ -0,0 +1,24 @@
+const dfs = function (node, min, max) {
+  if (node.val <= min || node.val >= max) {
+    return false;
+  }
+
+  if (node.left) {
+    if (!dfs(node.left, min, node.val)) {
+      return false;
+    }
+  }
+
+  if (node.right) {
+    if (!dfs(node.right, node.val, max)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isValidBST = function (root) {
+  if (!root) return true;
+  return dfs(root, -Infinity, Infinity);
+};
