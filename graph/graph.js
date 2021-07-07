f750bba7febaa2fbce170e4130c947cfa6c605f3
sonphan96 <phantruongson96@gmail.com>
2021-07-07 16:12:22 -0400

    DFS graphs

--- graph/graph.js
+++ graph/graph.js
@@ -22,7 +22,7 @@ const traversalBFS = function (graph) {
     values.push(vertex);
     seen[vertex] = true;
 
-    const connections = graph[vertex];
+    const connections = graph[vertex]; // 1 3
     for (let i = 0; i < connections.length; i++) {
       const connection = connections[i];
       if (!seen[connection]) {
@@ -73,3 +73,65 @@ const traversalBFS = function (graph) {
 };
 
 console.log(traversalBFS(adjacencyMatrix));
+
+// DFS List
+const adjacencyList = [
+  [1, 3],
+  [0],
+  [3, 8],
+  [0, 2, 4, 5],
+  [3, 6],
+  [3],
+  [4, 7],
+  [6],
+  [2],
+];
+
+const traversalDFS = function (vertex, graph, values, seen) {
+  values.push(vertex);
+  seen[vertex] = true;
+
+  const connections = graph[vertex];
+  for (let i = 0; i < connections.length; i++) {
+    const connection = connections[i];
+
+    if (!seen[connection]) {
+      traversalDFS(connection, graph, values, seen);
+    }
+  }
+};
+
+const values = [];
+traversalDFS(0, adjacencyList, values, {});
+
+console.log(values);
+
+// DFS Matrix
+const adjacencyMatrix = [
+  [0, 1, 0, 1, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 1, 0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 1, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0],
+];
+
+const traversalDFS = function (vertex, graph, values, seen) {
+  values.push(vertex);
+  seen[vertex] = true;
+
+  const connections = graph[vertex];
+  for (let v = 0; v < connections.length; v++) {
+    if (connections[v] > 0 && !seen[v]) {
+      traversalDFS(v, graph, values, seen);
+    }
+  }
+};
+
+const values = [];
+traversalDFS(0, adjacencyMatrix, values, {});
+
+console.log(values);
