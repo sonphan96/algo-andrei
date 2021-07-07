0e01423c8149bcda55f5234cc6ed41a1334c1514
sonphan96 <phantruongson96@gmail.com>
2021-07-07 16:05:29 -0400

    graphs BFS List

---
+++ graph/graph.js
@@ -0,0 +1,75 @@
+// BFS List
+const adjacencyList = [
+  [1, 3],
+  [0],
+  [3, 8],
+  [0, 2, 4, 5],
+  [3, 6],
+  [3],
+  [4, 7],
+  [6],
+  [2],
+];
+
+const traversalBFS = function (graph) {
+  const seen = {};
+  const queue = [0];
+  const values = [];
+
+  while (queue.length) {
+    const vertex = queue.shift();
+
+    values.push(vertex);
+    seen[vertex] = true;
+
+    const connections = graph[vertex];
+    for (let i = 0; i < connections.length; i++) {
+      const connection = connections[i];
+      if (!seen[connection]) {
+        queue.push(connection);
+      }
+    }
+  }
+
+  return values;
+};
+
+console.log(traversalBFS(adjacencyList));
+
+// BFS matrix
+
+const adjacencyMatrix = [
+  [0, 1, 0, 1, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 1, 0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 1, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0],
+];
+
+const traversalBFS = function (graph) {
+  const seen = {};
+  const queue = [0];
+  const values = [];
+
+  while (queue.length) {
+    const vertex = queue.shift();
+
+    values.push(vertex);
+    seen[vertex] = true;
+
+    const connections = graph[vertex];
+    for (let v = 0; v < connections.length; v++) {
+      if (connections[v] > 0 && !seen[v]) {
+        queue.push(v);
+      }
+    }
+  }
+
+  return values;
+};
+
+console.log(traversalBFS(adjacencyMatrix));
