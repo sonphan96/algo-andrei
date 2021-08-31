7bb65d0a5175a9da0ee19047305187ee818ebcca
sonphan96 <phantruongson96@gmail.com>
2021-08-30 20:05:00 -0400

    hash table object

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -1,6 +1,7 @@
 class HashTable {
   constructor(size) {
     this.data = new Array(size);
+    // this.data = [];
   }
 
   _hash(key) {
@@ -9,6 +10,39 @@ class HashTable {
       hash = (hash + key.charCodeAt(i) * i) % this.data.length;
     }
     return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    const address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    const keysArray = [];
+    console.log(this.data.length);
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
   }
 }
 
